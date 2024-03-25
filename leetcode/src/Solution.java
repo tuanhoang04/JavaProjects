f9047b5e5f9645eef4111e5d7ad9b3c57caaba0d
Tuan Hoang <hoangngoctuan2611@gmail.com>
2024-03-25 09:50:44 +0000

    solution change

--- leetcode/src/Solution.java
+++ leetcode/src/Solution.java
@@ -1,41 +1,79 @@
 class Solution {
-    // Returns maximum sum in
-    // a subarray of size k.
-    static int maxSum(int arr[], int n, int k)
-    {
-        // n must be greater
-        if (n < k) {
-            System.out.println("Invalid");
-            return -1;
+    public int findDuplicate(int[] nums) {
+        int[] copy = nums;
+        sort(copy, 0, copy.length-1);
+        for(int i = 1; i<copy.length;i++){
+            if(copy[i]==copy[i-1]){
+                return copy[i];
+            }
         }
+        return -1;
+}
+void merge(int arr[], int l, int m, int r)
+{
+    // Find sizes of two subarrays to be merged
+    int n1 = m - l + 1;
+    int n2 = r - m;
 
-        // Compute sum of first window of size k
-        int max_sum = 0;
-        for (int i = 0; i < k; i++)
-            max_sum += arr[i];
+    // Create temp arrays
+    int L[] = new int[n1];
+    int R[] = new int[n2];
 
-        // Compute sums of remaining windows by
-        // removing first element of previous
-        // window and adding last element of
-        // current window.
-        int window_sum = max_sum;
-        for (int i = k; i < n; i++) {
-            window_sum += arr[i] - arr[i - k];
-            max_sum = Math.max(max_sum, window_sum);
+    // Copy data to temp arrays
+    for (int i = 0; i < n1; ++i)
+        L[i] = arr[l + i];
+    for (int j = 0; j < n2; ++j)
+        R[j] = arr[m + 1 + j];
+
+    // Merge the temp arrays
+
+    // Initial indices of first and second subarrays
+    int i = 0, j = 0;
+
+    // Initial index of merged subarray array
+    int k = l;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
         }
-
-        return max_sum;
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
     }
 
-    // Driver code
-    public static void main(String[] args)
-    {
-        int arr[] = { 1, 4, 2, 10, 2, 3, 1, 0, 20 };
-        int k = 4;
-        int n = arr.length;
-        System.out.println(maxSum(arr, n, k));
+    // Copy remaining elements of L[] if any
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    // Copy remaining elements of R[] if any
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
     }
 }
 
-// This code is contributed
-// by  prerna saini.
+// Main function that sorts arr[l..r] using
+// merge()
+void sort(int arr[], int l, int r)
+{
+    if (l < r) {
+
+        // Find the middle point
+        int m = l + (r - l) / 2;
+
+        // Sort first and second halves
+        sort(arr, l, m);
+        sort(arr, m + 1, r);
+
+        // Merge the sorted halves
+        merge(arr, l, m, r);
+    }
+}
+}
